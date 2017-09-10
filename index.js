5a48e9f0b457dfc203cf3208a19c03962d5a97de
Anthony Fields <janthonyfields@gmail.com>
2017-09-10 02:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function theBeatlesPlay( musicians, instruments ) {
   var arr = [];
+
+  for ( let i = 0; i < musicians.length; i++ ) {
+    arr.push( `${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return arr;
 }
